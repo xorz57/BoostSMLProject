74a96c1b174e94cb409771ae98c6a713877b5124
George Fotopoulos <84932056+xorz57@users.noreply.github.com>
2023-03-11 01:59:37 +0200

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,14 +6,7 @@
 int main() {
     Logger logger;
     sml::sm<Transitions, sml::logger<Logger>> sm{logger};
-    assert(sm.is(sml::state<State0>));
-
     sm.process_event(Event1{true});
-    assert(sm.is(sml::state<State1>));
-
     sm.process_event(Event2{true});
-    assert(sm.is(sml::state<State2>));
-
     sm.process_event(EventX{true});
-    assert(sm.is(sml::X));
 }
